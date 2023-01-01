9b1a70d0c72dc7ff6f6bdffbe68c369152e2ba00
Yoav Ganbar <yoav@builder.io>
2023-01-01 13:34:45 +0200

    remove isomorphic-fetch

--- src/routes/api/auth/twitter/callback/index.tsx
+++ src/routes/api/auth/twitter/callback/index.tsx
@@ -1,22 +1,21 @@
-import { component$ } from "@builder.io/qwik";
-import { RequestHandler } from "@builder.io/qwik-city";
-import "isomorphic-fetch";
+import { component$ } from '@builder.io/qwik';
+import { RequestHandler } from '@builder.io/qwik-city';
 
-import { Button } from "~/components/Button";
-import { Logo } from "~/components/Logo";
-import { getToken } from "~/util/oauth";
+import { Button } from '~/components/Button';
+import { Logo } from '~/components/Logo';
+import { getToken } from '~/util/oauth';
 
 export default component$(() => {
   /** @todo error handling */
   return (
-    <div class="w-screen h-screen flex items-center justify-center flex-col gap-16">
+    <div class='w-screen h-screen flex items-center justify-center flex-col gap-16'>
       <Logo size={256} />
-      <div className="grid max-w-screen-md gap-4 mx-auto text-center">
-        <h1 className="text-6xl font-bold">You're All Set!</h1>
-        <p className="text-xl">Great, you're logged in and ready to go!</p>
+      <div className='grid max-w-screen-md gap-4 mx-auto text-center'>
+        <h1 className='text-6xl font-bold'>You're All Set!</h1>
+        <p className='text-xl'>Great, you're logged in and ready to go!</p>
       </div>
-      <div className="flex items-center gap-4">
-        <Button color="secondary" onClick$={() => (window.location.href = "/")}>
+      <div className='flex items-center gap-4'>
+        <Button color='secondary' onClick$={() => (window.location.href = '/')}>
           Go Home
         </Button>
       </div>
@@ -25,14 +24,14 @@ export default component$(() => {
 });
 
 export const onGet: RequestHandler = async ({ url, cookie }) => {
-  const authCode = url.searchParams.get("code")!;
-  const redirectUri = "http://localhost:5173/api/auth/twitter/callback";
+  const authCode = url.searchParams.get('code')!;
+  const redirectUri = 'http://localhost:5173/api/auth/twitter/callback';
 
   try {
     const token = await getToken({ authCode, redirectUri });
 
     cookie.set(
-      "token",
+      'token',
       JSON.stringify({
         value: token.access_token,
         expiresAt: new Date(Date.now() + token.expires_in * 1000),
@@ -40,7 +39,7 @@ export const onGet: RequestHandler = async ({ url, cookie }) => {
       {
         httpOnly: true,
         maxAge: token.expires_in,
-        path: "/",
+        path: '/',
       }
     );
 
